968bdf2977785d442f3fd07c9f2b5ef58000e4e6
erwinsnijder <48627520+erwinsnijder@users.noreply.github.com>
2019-05-13 12:02:21 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,95 @@
+class leerling{
+    constructor (name, wiskunde, biologie, engels, gym) {
+    this.name =name;
+    this.wiskunde = wiskunde;
+    this.biologie = biologie;
+    this.engels =  engels;
+    this.gym = gym;     
+}
+}
+
+let leerling1 = new leerling('Suus',  5, 8, 6, 7);
+let leerling2 = new leerling('Kees',  6, 7, 9, 3);
+let leerling3 = new leerling('Bep',   7, 3, 6, 7) ;
+let leerling4 = new leerling('Koos',  7, 3, 6, 8) ;
+let leerling5 = new leerling('Erik',  7, 3, 6, 8) ;
+let leerling6 = new leerling('Tim',   7, 3, 6, 6) ;
+
+console.log(leerling1, name, engels);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // }
+
+// class leerling {
+//     constructor(name, wiskunde, biologie, engelsl) {
+//         this.name = Suus;
+//         this.wiskunde = 8;
+//         this.biologie = 6;
+//         this.engels =   5;
+//     }
+// }
+
+// class leerling {
+//     constructor(name, wiskunde, biologie, engels) {
+//         this.name = Erik;
+//         this.wiskunde = 6;
+//         this.biologie = 7;
+//         this.engels =   8;
+//     }
+// }
+// class leerling {
+//     constructor(name, wiskunde, biologie, engels) {
+//         this.name = Wim;
+//         this.wiskunde = 3;
+//         this.biologie = 8;
+//         this.engels =   6;
+//     }
+// }
+// class leerling {
+//     constructor(name, wiskunde, biologie, engels) {
+//         this.name = Kees;
+//         this.wiskunde = 5;
+//         this.biologie = 6;
+//         this.engels =   7;
+//     }
+// }
+// class leerling {
+//     constructor(name, wiskunde, biologie, engels) {
+//         this.name = Kor;
+//         this.wiskunde = 7;
+//         this.biologie = 5;
+//         this.engels =   4;
+//     }
+// }
+// class leerling {
+//     constructor(name, wiskunde, biologie, engels) {
+//         this.name = Bep;
+//         this.wiskunde = 7;
+//         this.biologie = 5;
+//         this.engels =   4;
+//     }
+// }
+
+
