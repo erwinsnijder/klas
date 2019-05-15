b5d2c06ef88a33706a273ca4e4c56db147cd2240
erwin snijder <erwinsnijder_4@hotmail.com>
2019-05-15 14:34:39 +0200

    moved for loop to on click

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ var words = ['Fawn','Terina', 'Faviola', 'Maxwell', 'Lanie', 'Elfreda', 'Yu', 'D
  'Karmen', 'Celia', 'Annelle', 'inifred', 'Barbara', 'Ashanti', 'Roxie', 'Felice',
   'Calvin', 'Ingeborg', 'Adrian', 'Kees','Berta', 'Koos', 'Erik', 'Albert']
 
-
 class leerling{
     constructor () {
     this.anaam          = words[Math.floor(Math.random() * words.length)];
@@ -28,27 +27,20 @@ let leerling8  = new  leerling;
 let leerling9  = new  leerling;
 let leerling10 = new  leerling;
 
-
-
-
-
 let mijnklas = [leerling1, leerling2, leerling3];
 
 mijnklas.push(leerling4, leerling5, leerling6, leerling7, leerling8, leerling9, leerling10);
 
-// console.log(mijnklas);
-
-    //     function myFunction() {
-    //         document.getElementById("cijfers").innerHTML = (mijnklas);
-    // }
+   for (var x=0; x < mijnklas.length; x++) {
+//     console.log (mijnklas[x]);
+//    }  
+ 
+       function myFunction() {
+            document.getElementById("cijfers").innerHTML = (mijnklas);
+    }
+}
 //    for (let leerling in mijnklas) {
 //     function myFunction() {
 //     document.getElementById("cijfers").innerHTML = (leerling);
 //     }
 //    }
-   for (var x=0; x <= 9; x = x + 1) {
-    console.log (mijnklas[x]);}
-    //     function myFunction() {
-//         document.getElementById("cijfers").innerHTML = [x];
-// }
-        
